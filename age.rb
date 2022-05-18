a6b7d55ba40b48d77917c666374b5c94decc08e1
Yohana94 <qelqh90126@yahoo.co.jp>
2022-05-18 22:04:27 +0000

    second step for adding

---
+++ age.rb
@@ -0,0 +1,6 @@
+puts "Sam is" + 27 + "this year"
+#"sam is" is string type
+#27 is integer type
+#"this year" is string type
+#it cann`t convert and will show an error 「no implicit conversion of Integer into String 」
+puts "Samの年齢は" + 27 . to_s + "です"
